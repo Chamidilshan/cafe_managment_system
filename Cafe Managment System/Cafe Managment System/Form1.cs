361bff351579688a2e366dcc727b07a410423989
Chamika Dilshan <chamidilshan55@gmail.com>
2023-07-30 20:00:44 +0530

    implemented login page

--- Cafe Managment System/Cafe Managment System/Form1.cs
+++ Cafe Managment System/Cafe Managment System/Form1.cs
@@ -6,5 +6,25 @@ namespace Cafe_Managment_System
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
