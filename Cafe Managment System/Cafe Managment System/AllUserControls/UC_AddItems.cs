d371be5716e5e0f05db248a3f3d5279c7f0baa98
Chamika Dilshan <chamidilshan55@gmail.com>
2023-08-07 20:36:56 +0530

    added item add controller to dashboard

---
+++ Cafe Managment System/Cafe Managment System/AllUserControls/UC_AddItems.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cafe_Managment_System.AllUserControls
+{
+    public partial class UC_AddItems : UserControl
+    {
+        public UC_AddItems()
+        {
+            InitializeComponent();
+        }
+
+        private void UC_AddItems_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
